b847347f461a62db6942c002a765644c0de6a082
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2018-12-03 17:21:27 +0200

    Adding logOut servlet unit tests

--- src/main/java/com/chaika/servlet/LogoutServlet.java
+++ src/main/java/com/chaika/servlet/LogoutServlet.java
@@ -14,12 +14,11 @@ import java.io.IOException;
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final HttpSession session = req.getSession();
         session.removeAttribute("login");
         session.removeAttribute("password");
         session.removeAttribute("role");
-        System.out.println(req.getContextPath());
 
         resp.sendRedirect("/");
     }
---
+++ src/test/java/LogoutServletTest.java
@@ -0,0 +1,54 @@
+import com.chaika.model.enums.Role;
+import com.chaika.servlet.LogoutServlet;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by echaika on 03.12.2018
+ */
+public class LogoutServletTest {
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private HttpSession httpSession;
+    @Mock
+    private RequestDispatcher requestDispatcher;
+
+    private static final String path = "/view/login.jsp";
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void whenLogout() throws ServletException, IOException {
+        when(request.getParameter("user")).thenReturn("Evgeniy");
+        when(request.getParameter("password")).thenReturn("1");
+        when(request.getParameter("rememberMe")).thenReturn(String.valueOf(Role.ADMIN));
+        when(request.getSession()).thenReturn(httpSession);
+
+        when(request.getRequestDispatcher("/")).thenReturn(requestDispatcher);
+
+        LogoutServlet logoutServlet = new LogoutServlet();
+        logoutServlet.doGet(request, response);
+
+        verify(request).getSession();
+        verify(request, times(1)).getSession();
+        verify(response).sendRedirect("/");
+        verify(response, times(1)).sendRedirect("/");
+    }
+}
